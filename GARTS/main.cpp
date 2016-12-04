6101963595883885cee73186efe2bdcfa7fdba94
Sam Cumming <forsamori@yahoo.co.uk>
2016-12-04 16:04:35 +0000

    SDL Function Test - More for my own benefit in making sure set up is complete.

--- GARTS/main.cpp
+++ GARTS/main.cpp
@@ -1,12 +1,91 @@
-#include "SDL.h"
+//#include "SDL.h"
+#include "main.h"
 
 int main(int argc, char* args[])
 {
 	//Start SDL
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	//Quit SDL
-	SDL_Quit();
-
+	if (!init())
+	{
+		printf("FAILED TO INITIALIZE!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Apply image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			//Update surface
+			SDL_UpdateWindowSurface(gWindow);
+			//Wait two secs
+			SDL_Delay(2000);
+		}
+	}
+	
+	close();
 	return 0;
+}
+
+bool init()
+{
+	//Success Flag
+	bool success = true;
+
+	//Initialise SDL 
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) //Init fail
+	{
+		printf("SDL could not initialize! SDL_ERROR: %s\n", SDL_GetError());
+		success = false;
+	}
+	else //INIT SUCCESS
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("GARTS", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get Window Surface so we can draw on the window.
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success
+	bool success = true;
+
+	
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("C:/Users/Sam/Pictures/lol.bmp");
+	if (gHelloWorld == NULL) //Unable to load image
+	{
+		printf("Unable to load image %s! SDL_ERROR: %s\n", "C:/Users/Sam/Pictures/lol.bmp", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
 }
--- GARTS/main.h
+++ GARTS/main.h
@@ -1,6 +1,41 @@
+//The following implementation is currently set up to...
+//...render using software on the CPU. Swap to GPU to allow...
+//...CPU to concentrate on game-logic and GA...
+
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include "SDL.h"
+#include <stdio.h>
+
+//Const definitions
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main();
+
+//Initialise App: Set up SDL and create Window
+bool init();
+
+//Loads media files
+bool loadMedia();
+
+//Frees up media and shuts down SDL
+void close();
+
+//Declare SDL_ vars
+
+// --- Global Variables ---
+
+//Render Window
+SDL_Window* gWindow = NULL;
+
+//Window Surface (The surface we'll draw to)
+SDL_Surface* gScreenSurface = NULL;
+
+//Image to Render
+SDL_Surface* gHelloWorld = NULL;
+
+// --- END Global Variables ---
 
 #endif
