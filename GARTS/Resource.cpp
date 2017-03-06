96f79322d40623c3837ee799e79ee2630df4b339
Sam Cumming <forsamori@yahoo.co.uk>
2017-03-06 16:36:01 +0000

    Create Resource class
    Resource class is a basic implementation of a resource object.
    Unit_Worker objects can touch it (while not carrying nay resources) and
    collect an allocation of resources.

---
+++ GARTS/Resource.cpp
@@ -0,0 +1,37 @@
+#include "Resource.h"
+
+Resource::~Resource()
+{
+}
+
+int Resource::Harvest()
+{
+	resourceRemaining -= harvestAmount;
+	return harvestAmount;
+}
+
+void Resource::Update()
+{
+	
+	/*for (int i = 0; i < gameObjectsRef->size(); i++)
+	{
+		if (AABBCollision(this, gameObjectsRef->at(i)))
+		{
+			if (gameObjectsRef->at(i)->OT == OT_RESOURCE)
+
+		}
+	}*/
+	//Execute generic GameObject update
+	GameObject::Update();
+}
+
+
+void Resource::Render()
+{
+
+	if (resourceRemaining >= 0)
+	{//HACK
+	//Execute generic GameObject render
+		GameObject::Render();
+	}
+}
---
+++ GARTS/Resource.h
@@ -0,0 +1,31 @@
+#ifndef _RESOURCE_H
+#define _RESOURCE_H
+
+#include "GameObject.h"
+
+class Resource : public GameObject
+{
+
+public:
+
+	Resource(std::string tex_path, SDL_Renderer* _renderer, std::vector<GameObject*>* _gameObjectsRef, std::vector<GameObject>* _gameObjects) : GameObject(tex_path, _renderer, _gameObjectsRef, _gameObjects)
+	{
+		OT = OT_RESOURCE;
+	}
+	~Resource();
+	int Harvest();
+
+	void Update();
+	void Render();
+
+protected:
+
+private:
+
+	int harvestAmount = 5;
+	int resourceRemaining = 100;
+
+
+};
+
+#endif
